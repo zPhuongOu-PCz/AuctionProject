d5c05b78c823e784598862c7ecfec559643c5cfa
Nguyễn Ngọc Phương <1451010140Phuong@ou.edu.vn>
2017-12-07 12:01:13 +0700

    Create.Project

---
+++ AuctionProject/Auction.Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Auction.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid IDuser { get; set; }
+        [Required]
+        public string username { get; set; }
+        [Required]
+        public string password { get; set; }
+
+        public User()
+        {
+            this.IDuser = Guid.NewGuid();
+            this.username = string.Empty;
+            this.password = string.Empty;
+        }
+
+        public User(string _us, string _ps)
+        {
+            this.IDuser = Guid.NewGuid();
+            this.username = _us;
+            this.password = _ps;
+        }
+    }
+}
