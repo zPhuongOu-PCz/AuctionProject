5e3079a31658d86d8192a978b2a42d1a4e59fa9c
Nguyễn Ngọc Phương <34095199+zPhuongOu-PCz@users.noreply.github.com>
2018-01-21 14:51:44 +0700

    Update Project Alpha v1.8.1
    - Update Cors

--- AuctionProject/Auction.Service/Startup.cs
+++ AuctionProject/Auction.Service/Startup.cs
@@ -28,8 +28,13 @@ namespace Auction.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
             services.AddMvc();
-            services.AddCors();
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
 
@@ -42,6 +47,8 @@ namespace Auction.Service
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors("MyPolicy");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
